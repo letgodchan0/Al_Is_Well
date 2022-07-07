6846dfabd29098126c636b74fdde84a7caa6f771
zzz0105 <lisajieun@naver.com>
2022-07-07 21:15:11 +0900

    Add 구간 나누기 2

---
+++ 5주차/0704/지은/구간 나누기 2.py
@@ -0,0 +1,27 @@
+# 이분탐색 기준값(mid)는 각 구간의 (최댓값-최솟값) 의 최댓값 중 최솟값으로 정의(=result)
+n,m=map(int,input().strip().split())
+arr=list(map(int,input().strip().split()))
+
+def divide(x):
+  max_x=min_x=arr[0]
+  cnt=1
+  for i in range(1,n):
+    max_x=max(max_x,arr[i])
+    min_x=min(min_x,arr[i])
+    if max_x - min_x > x:
+      cnt+=1
+      max_x=arr[i]
+      min_x=arr[i]
+  return cnt
+
+start, end = 0, max(arr)
+result=0
+while start<=end:
+  mid=(start+end)//2
+  if divide(mid)<=m:
+    end = mid-1
+    result=mid
+  else:
+    start = mid+1
+
+print(result)
