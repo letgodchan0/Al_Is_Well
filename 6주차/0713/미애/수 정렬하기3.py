b6124dfb19895494e75ae77271c57c3ea90548a4
aldoremiae-e <miaemoo83@gmail.com>
2022-07-10 22:14:55 +0900

    Add 수 정렬하기 3

---
+++ 6주차/0713/미애/수 정렬하기3.py
@@ -0,0 +1,11 @@
+import sys
+N = int(sys.stdin.readline())
+arr = [0] * 10001
+
+for _ in range(N):
+    arr[int(sys.stdin.readline())] += 1
+
+for i in range(10001):
+    if arr[i] != 0:
+        for j in range(arr[i]):
+            print(i)
